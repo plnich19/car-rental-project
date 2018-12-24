3fd27ea183dee27f626d6456140dc54552c1435e
Nichakan Udomlerkchai <PloyNichakan@gmail.com>
2018-12-24 10:07:10 +0700

    Create howtoreserve.php

---
+++ howtoreserve.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reservation</title>
+<link rel="stylesheet" href="help.css">
+</head>
+
+<body>
+<header>
+            <span class="menu">
+                <table  align="center" height="100"> 
+                    <tr>
+                    <td class="btn" width="200px" id="main"><a href="index.php">Main</td>
+                    <td class="btn" width="200px" id="car"><a href="car.php">Our Car</td>
+                        <td class="btn" width="200px" id="res"><a href="howtoreserve.php">Reservation</td>
+                        <td class="btn" width="200px" id="care"><a href="cuscare.php">Customer Care</td>
+                        <td class="btn" width="200px" id="pro"><a href="promo.php">Promotion</td>
+                        <td class="btn" width="200px" id="brm"><a href="Map.php">Our Branches</td>
+                        <td class="btn" width="200px" id="helpm"><a href="help.php">Help</td>
+                    </tr>
+                </table>
+            </span>
+        </header>
+        <br>
+        <!-- <span class="sub">Register to receive our newsletter<br><input type="email" name="email" placeholder="fill in your email"><button type="submit" name="submit">Submit</button></span>         -->
+        <br>
+        <br>
+<main>
+		<div id="div_content">
+            <h2>How to reserve</h2>
+            <p>------------------------------------------------------</p>
+            <h3>First step</h3>
+            <p>Choose car you want to reserve <a href="car.php">here</a> by clicking reserve button</p>
+            <p>------------------------------------------------------</p>
+            <h3>Second step</h3>
+            <p>If you already chose your favorite car, the website will be redirect automatically to requiring customer information page. Please fill in your information before click 'Confirm' reserve button.</p>
+            <p>------------------------------------------------------</p>
+            <h3>Last step</h3>
+            <p>Your reservation will be recoded automatically in our database. :)</p>
+            <p>------------------------------------------------------</p>
+            <h3>PS.</h3>
+            <p>You can require cancellation via <a href="cuscare.php">this link</a></p>
+            <p>About other terms follow often <a href="help.html">Q&A page here</a></p>
+
+    </div>
+</main>
+<footer>
+        <p>© Car Rental ©</p>
+        <p>Address 56/1 Street</p>
+        <!-- <img src="bloggif_5bb8bc2d97d96.png" width="50" height="50"><img id="twit"src=bloggif_5bb8bd9be8059.png width="50" height="50"> -->
+</footer>
+</body>
